859ddcd3d09990cb0f1f6c25d4949d833e228c2e
codecobber <codecobber@gmail.com>
2016-10-09 15:24:37 +0100

    base_project_footer

---
+++ projectName_footer.php
@@ -0,0 +1,154 @@
+
+
+<div id="myModal" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog"><h1>Search content within project Name</h1>
+<script>
+  //google search code goes here
+</script>
+<gcse:search></gcse:search>
+</div>
+
+
+<footer class="footer" id="greyFooter">
+  <div class="row hide-for-medium-down">
+			<div class="xlarge-3 large-3 medium-3 small-6 columns">
+				<nav>
+					<ul class="">
+						<li><h3>More...</h3></li>
+				      <li><a href="<?php echo SITEROOT.'about.php'; ?>" title="Who are project Name" >So who are project Name?</a></li> 
+						<li><a href="<?php echo SITEROOT.'more.php'; ?>" title="What do you need?" >More for your visitor </a></li> 
+						<li><a href="<?php echo SITEROOT.'what_now.php'; ?>" title="What happens next?" >How to get started</a></li>
+						<li><a href="<?php echo SITEROOT.'support.php'; ?>" title="Support - helping you when it matters" >Customer Support</a></li>
+	               <li><a href="<?php echo SITEROOT.'portfolio.php'; ?>" title="projectName Portfolio - our designs and creations" >Portfolio</a></li>  
+	                 
+					</ul>
+				</nav>
+			</div><!-- /column -->
+		
+			<div class="xlarge-3 large-3 medium-3 columns">
+				<nav>
+					<ul class="">
+					 	<li><h3>Web design</h3></li>
+						<li><a href="<?php echo SITEROOT.'web_services.php'; ?>" title="We design and develop web sites tailored to your business &#8211; Town">Web services</a></li>		
+						<li><a href="<?php echo SITEROOT.'seo.php'; ?>" title="SEO matters because it&#39;s all about being seen &#8211; Town" >SEO - getting seen</a></li> 
+						<li><a href="<?php echo SITEROOT.'website_pricing.php'; ?>" title="project Name - clear, transparent pricing &#8211; Town" >Web site pricing</a></li>
+						<li><a href="<?php echo SITEROOT.'cms.php'; ?>" title="project Name - Uses Concrete 5 cms &#8211; Town" >Concrete5 CMS</a></li>  
+						<li><a href="<?php echo SITEROOT.'maintenance.php'; ?>" title="project Name web maintenance &#8211; Town" >Web Maintenence</a></li>
+					</ul>
+				</nav>
+			</div><!-- /column -->
+			
+			
+		 <div class="xlarge-3 large-3 medium-3 small-6 columns">
+				<nav>
+					 <ul>
+						<li><h3>Logo design</h3></li>
+						<li><a href="<?php echo SITEROOT.'why_logo.php'; ?>" title="Why would you require a logo?">Why would I require a logo?</a></li>		
+						<li><a href="<?php echo SITEROOT.'what_logo.php'; ?>" title="How do project Name design a logo?" >What does it involve?</a></li> 
+						<li><a href="<?php echo SITEROOT.'logos.php'; ?>" title="Take a look at some of the logos we&#38;ve designed" >Logo examples</a></li>
+						<li><a href="<?php echo SITEROOT.'logos_in_action.php'; ?>" title="Take a look at some of the logos on different items, from cups to pen drives - great advertising!" >Logos in action</a></li>  
+						<li><a href="<?php echo SITEROOT.'logo_prices.php'; ?>" title="The price of a logo" >Logo prices</a></li>
+				    </ul>
+		      </nav>
+			</div><!-- /column -->
+		
+		
+		<div class="xlarge-3 large-3 medium-3 small-6 columns">
+	        <nav>			
+					<ul>
+							<li><h3>More links</h3></li>
+							<li><a href="<?php echo SITEROOT; ?>contact.php" title="Site usage" >Contact project Name</a></li> 
+							<li><a href="<?php echo SITEROOT.'site_terms.php'; ?>" title="project Name copyright and site usage" >Site terms</a></li>
+							<li><a href="<?php echo SITEROOT.'free.php'; ?>" title="Free software from the open source community" >Free software</a></li>
+			            <li><a href="<?php echo SITEROOT.'blog.php'; ?>" title="project Name blog" >Blog</a></li>
+			            <li><a href="<?php echo SITEROOT.'terms.php'; ?>" title="Design terms - our terms and conditions" >Design Terms</a></li>
+			            
+					</ul>
+			   </nav>
+		</div><!-- /column -->
+</div><!-- /row -->
+
+
+					
+
+<div class="footerBase">
+      
+	  
+	<div class="row">
+		<div class="large-12 hide-for-small large-center text-center columns">
+		<hr>
+		<a href="<?php echo SITEROOT.'index.php'; ?>">Home</a> | <a href="<?php echo SITEROOT.'about.php';?>">About us</a> | <a href="<?php echo SITEROOT.'web_services.php';?>">Services</a> | <a href="<?php echo SITEROOT.'terms.php';?>">Terms</a> | <a href="<?php echo SITEROOT.'cookie_policy.php';?>">Cookies</a> | <a href="<?php echo SITEROOT.'contact.php';?>">Contact</a> 
+		<hr>
+        <div class="row" id="social">
+            <div class="large-12 large-center columns">
+            <a href="https://twitter.com/projectName"><i class="fa  fa-twitter-square fa-2x"></i></a> | <a href="google + code here"><i class="fa fa-google-plus-square fa-2x"></i></a>
+            </div>
+        </div>
+    
+		<small>Copyright 2014 - project Name</small>
+		</div>
+        
+        <div class="small-12 show-for-small text-center columns">
+		<hr>
+		<a href="<?php echo SITEROOT.'index.php'; ?>">Home</a> | <a href="<?php echo SITEROOT.'about.php';?>">About us</a> | <a href="<?php echo SITEROOT.'web_services.php';?>">Services</a> | <a href="<?php echo SITEROOT.'terms.php';?>">Terms</a> | <a href="<?php echo SITEROOT.'cookie_policy.php';?>">Cookies</a> | <a href="<?php echo SITEROOT.'contact.php';?>">Contact</a> 
+		<hr>
+		<small>Copyright <?php echo date('Y') ?>- project Name</small>
+		</div>
+	</div>
+    
+    
+
+        
+		
+		
+</div>
+
+</footer>
+
+
+		
+		
+
+
+
+
+
+        <!-- This script makes IE versions able to use media queries -->
+        <script src="js/respond.min.js"></script>
+
+        <!-- All Foundation jQuery plugins -->
+        <script src="js/foundation.min.js"></script>
+
+        <!-- This is where Foundation and other plugins are called -->
+        <script>
+		    $(document).foundation();
+		  </script>
+		  
+		  
+<script>
+function newWindow(dest)
+{
+var goto = "http://www." + dest;
+window.open(goto);
+}
+</script>
+
+
+
+<script> 
+   $(document).ready(function(){
+   	function scrollToAnchor(aid){
+	    var aTag = $("div[id='"+ aid +"']");
+	    $('html,body').animate({scrollTop: aTag.offset().top},750);
+		}
+		
+		$("#startLink").click(function() {
+		   scrollToAnchor('startTop');
+		});
+   });
+</script>  
+   
+   
+
+
+</body>
+</html>
